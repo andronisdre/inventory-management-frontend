e404bf5b2aa622ed67c291edde7aabb207293d8c
andronisdre <andrebulow13@gmail.com>
2025-09-13 15:45:58 +0200

    improving design for User experience. still need to fix low stock fething

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -14,15 +14,18 @@ const ArticleList = ({
   const [loading, setLoading] = useState(true);
   const [watchingAllArticles, setWatchingAllArticles] = useState(true);
   const [nameSearch, setNameSearch] = useState("");
+  const [pageNumber, setPageNumber] = useState(0);
 
-  const fetchAllArticles = async () => {
+  const fetchAllArticles = async (page = pageNumber) => {
     try {
-      console.log("calls twice");
       setLoading(true);
       console.log("nameSearch", nameSearch);
+      console.log("pagenumber", page);
 
       const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/articles?search=${nameSearch}`
+        `${
+          import.meta.env.VITE_API_URL
+        }/articles?page=${page}&search=${nameSearch}`
       );
 
       if (!response.ok) {
@@ -95,12 +98,17 @@ const ArticleList = ({
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchAllArticles();
+    fetchAllArticles(0);
+  };
+
+  const handlePageClick = (i) => {
+    setPageNumber(i);
+    fetchAllArticles(i);
   };
 
   useEffect(() => {
     fetchAllArticles();
-  }, [refreshTrigger]);
+  }, [pageNumber, refreshTrigger]);
 
   const onChange = (e) => {
     setNameSearch(e.target.value);
@@ -112,128 +120,151 @@ const ArticleList = ({
 
   return (
     <div className="containerArticleList">
-      <h2>Inventory Management</h2>
+      <div>
+        <h2>Inventory Management</h2>
 
-      <button onClick={onShowCreateForm} className="createArticleButton">
-        Create Article
-      </button>
-
-      <form className="formSearchArticle" onSubmit={handleSubmit}>
-        <label className="searchInput">
-          <input
-            type="text"
-            name="minimumAmount"
-            placeholder="Search articles by name"
-            value={nameSearch}
-            onChange={onChange}
-          />
-        </label>
-        <button className="searchButton" type="submit">
-          Search <FaSearch />
+        <button onClick={onShowCreateForm} className="createArticleButton">
+          Create Article
         </button>
-      </form>
 
-      {watchingAllArticles ? (
-        <p>showing all articles</p>
-      ) : (
-        <p>showing articles with low stock</p>
-      )}
+        <form className="formSearchArticle" onSubmit={handleSubmit}>
+          <label className="searchInput">
+            <input
+              type="text"
+              name="minimumAmount"
+              placeholder="Search articles by name"
+              value={nameSearch}
+              onChange={onChange}
+            />
+          </label>
+          <button className="searchButton" type="submit">
+            Search <FaSearch />
+          </button>
+        </form>
 
-      {articles.length === 0 ? (
-        <p style={{ backgroundColor: "black" }}>No articles found.</p>
-      ) : (
-        <div className="articleListContainer">
-          <table className="articleList">
-            <thead>
-              <tr style={{ backgroundColor: "black" }}>
-                <th style={{ border: "1px solid white", padding: "8px" }}>
-                  Name
-                </th>
-                <th style={{ border: "1px solid white", padding: "8px" }}>
-                  Amount
-                </th>
-                <th style={{ border: "1px solid white", padding: "8px" }}>
-                  Min Amount
-                </th>
-                <th style={{ border: "1px solid white", padding: "8px" }}>
-                  Unit
-                </th>
-                <th style={{ border: "1px solid white", padding: "8px" }}>
-                  Status
-                </th>
-                <th style={{ border: "1px solid white", padding: "8px" }}>
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {articles.content.map((article) => (
-                <tr
-                  key={article.id}
-                  style={{
-                    backgroundColor: "black",
-                  }}
-                >
-                  <td style={{ border: "1px solid white", padding: "8px" }}>
-                    {article.name}
-                  </td>
-                  <td
-                    style={{
-                      border: "1px solid white",
-                      padding: "8px",
-                      color: article.lowStock ? "red" : "white",
-                      fontWeight: article.lowStock ? "bold" : "normal",
-                    }}
-                  >
-                    {article.amount}
-                  </td>
-                  <td style={{ border: "1px solid white", padding: "8px" }}>
-                    {article.minimumAmount}
-                  </td>
-                  <td style={{ border: "1px solid white", padding: "8px" }}>
-                    {article.unit.toLowerCase()}
-                  </td>
-                  <td
-                    style={{
-                      border: "1px solid white",
-                      padding: "8px",
-                      color: article.lowStock ? "red" : "green",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {article.lowStock ? "LOW STOCK" : "OK"}
-                  </td>
-                  <td style={{ border: "1px solid white", padding: "8px" }}>
-                    <button
-                      className="deleteArticleButton"
-                      onClick={() => {
-                        handleDeleteArticle(article.id);
-                      }}
-                      style={{ marginRight: "10px" }}
-                    >
-                      <MdDeleteForever />
-                    </button>
-                    <button
-                      className="updateArticleButton"
-                      onClick={() => handleUpdateClick(article)}
-                    >
-                      <GrDocumentUpdate />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        {watchingAllArticles ? (
+          <p className="currentDisplayText">showing all articles</p>
+        ) : (
+          <p className="currentDisplayText">showing articles with low stock</p>
+        )}
+
+        <div className="paginationContainer">
+          {Array.from({ length: articles.totalPages }, (_, i) => (
+            <button
+              key={i}
+              onClick={() => handlePageClick(i)}
+              className="pageButton"
+              style={{
+                backgroundColor: i === pageNumber ? "black" : "#1a1a1a",
+              }}
+            >
+              {i + 1}
+            </button>
+          ))}
         </div>
-      )}
-      <button onClick={fetchAllArticles} className="refreshArticlesButton">
-        Refresh All Articles
-      </button>
-      <button onClick={fetchArticlesWithLowAmount} className="lowStockButton">
-        Get Articles With Low stock
-      </button>
+        <button
+          onClick={() => fetchAllArticles()}
+          className="refreshArticlesButton"
+        >
+          Refresh All Articles
+        </button>
+        <button onClick={fetchArticlesWithLowAmount} className="lowStockButton">
+          Get Articles With Low stock
+        </button>
 
-      <div className="totalArticles">Total articles: {articles.totalItems}</div>
+        <div className="totalArticles">
+          Total articles: {articles.totalItems}
+        </div>
+      </div>
+      <div>
+        {articles.length === 0 ? (
+          <p style={{ backgroundColor: "black" }}>No articles found.</p>
+        ) : (
+          <div className="articleListContainer">
+            <table className="articleList">
+              <thead>
+                <tr style={{ backgroundColor: "black" }}>
+                  <th style={{ border: "1px solid white", padding: "8px" }}>
+                    Name
+                  </th>
+                  <th style={{ border: "1px solid white", padding: "8px" }}>
+                    Amount
+                  </th>
+                  <th style={{ border: "1px solid white", padding: "8px" }}>
+                    Min Amount
+                  </th>
+                  <th style={{ border: "1px solid white", padding: "8px" }}>
+                    Unit
+                  </th>
+                  <th style={{ border: "1px solid white", padding: "8px" }}>
+                    Status
+                  </th>
+                  <th style={{ border: "1px solid white", padding: "8px" }}>
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {articles.content.map((article) => (
+                  <tr
+                    key={article.id}
+                    style={{
+                      backgroundColor: "black",
+                    }}
+                  >
+                    <td style={{ border: "1px solid white", padding: "8px" }}>
+                      {article.name}
+                    </td>
+                    <td
+                      style={{
+                        border: "1px solid white",
+                        padding: "8px",
+                        color: article.lowStock ? "red" : "white",
+                        fontWeight: article.lowStock ? "bold" : "normal",
+                      }}
+                    >
+                      {article.amount}
+                    </td>
+                    <td style={{ border: "1px solid white", padding: "8px" }}>
+                      {article.minimumAmount}
+                    </td>
+                    <td style={{ border: "1px solid white", padding: "8px" }}>
+                      {article.unit.toLowerCase()}
+                    </td>
+                    <td
+                      style={{
+                        border: "1px solid white",
+                        padding: "8px",
+                        color: article.lowStock ? "red" : "green",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {article.lowStock ? "LOW STOCK" : "OK"}
+                    </td>
+                    <td style={{ border: "1px solid white", padding: "8px" }}>
+                      <button
+                        className="deleteArticleButton"
+                        onClick={() => {
+                          handleDeleteArticle(article.id);
+                        }}
+                        style={{ marginRight: "10px" }}
+                      >
+                        <MdDeleteForever />
+                      </button>
+                      <button
+                        className="updateArticleButton"
+                        onClick={() => handleUpdateClick(article)}
+                      >
+                        <GrDocumentUpdate />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
